4c326c8067bd04e8142001b917db6f3fbc7ecd59
ZSchmitt <zaiane.schmitt@gmail.com>
2022-12-18 12:35:10 +0100

    feat: home + config app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/typo.import.scss";
+import type { AppProps } from "next/app";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+import { borderRadius, fonts, shadow } from "../styles/variables";
+
+const StyledBg = styled.div`
+  display: flex;
+  max-width: 1200px;
+  margin: auto;
+  box-shadow: ${shadow.primary};
+  border-radius: ${borderRadius.tenpx};
+  overflow: hidden;
+  font-family: ${fonts.base};
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyledBg>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StyledBg>
+  );
 }
