9fdb1ffd17e2990ffcfd2ddf5f1b8c7fc345e83c
ZSchmitt <zaiane.schmitt@gmail.com>
2022-12-18 12:34:01 +0100

    feat: variables

---
+++ styles/variables.ts
@@ -0,0 +1,27 @@
+export const colors = {
+  primary: "#b9cc5f",
+  grayScales: {
+    gray800: "#343a40",
+    gray900: "#212529",
+    white: "#ffffff",
+    black: "#0f0f0f",
+  },
+};
+
+export const shadow = {
+  primary: "0px 4px 6px 0px rgba(0, 0, 0, 0.25)",
+};
+
+export const borderRadius = {
+  fivepx: "5px",
+  tenpx: "10px",
+};
+
+export const importFonts = {
+  base: "https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap",
+};
+
+export const fonts = {
+  base: "Source Sans Pro",
+  monospace: "Menlo, Monaco, Consolas, monospace",
+};
