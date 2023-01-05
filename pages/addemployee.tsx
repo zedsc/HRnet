4486efc4c523857b591917f6ce7b4181f8656db4
ZSchmitt <zaiane.schmitt@gmail.com>
2023-01-05 19:53:16 +0100

    feat: emplpyee page

---
+++ pages/addemployee.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import FormAddEmployee from "../components/form/Form";
+import Meta from "../components/Meta";
+import { borderRadius, colors, shadow } from "../styles/variables";
+
+const StyledBg = styled.div`
+  width: 95%;
+  height: 95%;
+  background-color: ${colors.grayScales.white};
+  margin: auto;
+  margin-top: 20px;
+  overflow: hidden;
+  border-radius: ${borderRadius.tenpx};
+  box-shadow: ${shadow.primary};
+`;
+
+const Title = styled.h2`
+  margin: 0px;
+`;
+
+const addemployee = () => {
+  return (
+    <StyledBg>
+      <Meta title={"Add an employee"} />
+      <Title>Add an employee</Title>
+      <FormAddEmployee />
+    </StyledBg>
+  );
+};
+
+export default addemployee;
