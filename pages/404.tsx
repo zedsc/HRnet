4deb8881a9ef7e1b3e8df88de20c5d93cf0896ac
ZSchmitt <zaiane.schmitt@gmail.com>
2023-01-20 18:54:49 +0100

    feat: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { fontSize } from "../styles/variables";
+
+const StyledTitle = styled.h2`
+  margin: 150px auto 0px auto;
+  font-size: ${fontSize[24]};
+  text-align: center;
+  @media (max-width: 600px) {
+    font-size: ${fontSize[18]};
+  }
+`;
+
+const ContainerTitle = styled.div`
+  display: flex;
+`;
+
+const Custom404 = () => {
+  return (
+    <ContainerTitle>
+      <StyledTitle>
+        Sorry ! The page you are looking for doesn't exist 😢
+      </StyledTitle>
+    </ContainerTitle>
+  );
+};
+
+export default Custom404;
