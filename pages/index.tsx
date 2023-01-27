9b6f39872b1990bebe80740480716c0af1fd676d
ZSchmitt <zaiane.schmitt@gmail.com>
2023-01-27 12:32:07 +0100

    refactor: styles, title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Meta from "../components/Meta";
+import styled from "styled-components";
+import { fontSize } from "../styles/variables";
 
-const index = () => {
+const StyledTitle = styled.h2`
+  margin: 150px auto 0px auto;
+  font-size: ${fontSize[48]};
+  text-align: center;
+  @media (max-width: 600px) {
+    font-size: ${fontSize[32]};
+  }
+`;
+
+const ContainerTitle = styled.div`
+  display: flex;
+`;
+
+const Index = () => {
   return (
-    <div>
+    <>
       <Meta />
-      <h1>Bienvenue sur HRnet, user</h1>
-    </div>
+      <ContainerTitle>
+        <StyledTitle>Welcome to HRnet</StyledTitle>
+      </ContainerTitle>
+    </>
   );
 };
 
-export default index;
+export default Index;
